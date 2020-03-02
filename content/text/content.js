667e75241f3630f3102c4977ec8b33550de19baa
AK-21 <srebrny021@gmail.com>
2020-03-02 10:56:51 +0100

    Changed images links to hosted on github

--- content/text/content.js
+++ content/text/content.js
@@ -10,7 +10,7 @@ function getContent() {
                 "header":"Lorem ipsum",
                 paragraphs:[
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit elit, interdum sed volutpat eget, rutrum vitae dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris in efficitur nisl. Morbi venenatis sapien sodales eros facilisis, vitae dapibus augue tempor. Sed dictum massa eget ante accumsan, at interdum lacus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus sed ornare justo. Proin aliquet sapien vel placerat feugiat. Pellentesque justo lectus, porta sit amet libero vel, ullamcorper vehicula erat. Cras cursus finibus nisi in tristique. Nam ligula ligula, sollicitudin ut lorem vel, aliquet consectetur orci. Fusce at lacus rutrum, pulvinar elit et, auctor nibh. Aenean condimentum magna nec nisi mollis, nec luctus urna hendrerit.",
-                    IMG+"content/images/surface.jpg",
+                    IMG+"https://user-images.githubusercontent.com/37296308/75664951-a22bdf00-5c73-11ea-8e71-e1c259707c1b.jpg",
                     "Nullam quis tortor et sapien semper mattis a eu metus. Aenean aliquam at nisl sed pulvinar. Vestibulum vel turpis hendrerit, fringilla purus et, finibus nunc. Aliquam erat volutpat. Vestibulum diam eros, imperdiet ut consequat non, aliquam vel lectus. Nullam imperdiet mattis orci, sit amet pellentesque dolor finibus vitae. Vivamus viverra ultricies magna in dignissim. Suspendisse in sollicitudin sem, vel condimentum enim. Aenean id nulla quis felis eleifend lacinia. In aliquam molestie felis, sed ultrices tellus porttitor sed. Suspendisse faucibus tempor sollicitudin. In sit amet euismod libero. Phasellus in pharetra dui, non aliquam enim. Maecenas ac velit vel ante tincidunt scelerisque."
                 ]
             },
@@ -18,7 +18,7 @@ function getContent() {
                 "header":"dolor sit amet",
                 paragraphs:[
                     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque at nibh dignissim arcu hendrerit aliquam non et neque. Vivamus iaculis feugiat odio, convallis faucibus urna hendrerit nec.",
-                    IMG+"content/images/pcb.jpg"+SUB+"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    IMG+"https://user-images.githubusercontent.com/37296308/75664950-a1934880-5c73-11ea-9baf-e6994173c4f1.jpg"+SUB+"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                     "Praesent efficitur neque urna, sit amet sollicitudin sapien venenatis volutpat. Nunc malesuada iaculis sollicitudin.",
                     "Morbi vel semper lacus. Fusce faucibus mi quis egestas finibus."
                 ]
@@ -27,9 +27,9 @@ function getContent() {
                 "header":"consectetur",
                 paragraphs:[
                     "Sed vulputate magna sed diam convallis, ut dapibus leo imperdiet. Quisque vulputate consequat faucibus. Donec placerat gravida rutrum. Pellentesque id mi diam.",
-                    IMG+"content/images/surface.jpg",
+                    IMG+"https://user-images.githubusercontent.com/37296308/75664951-a22bdf00-5c73-11ea-8e71-e1c259707c1b.jpg",
                     "Cras luctus rutrum velit, eget rhoncus ligula interdum sit amet. Pellentesque euismod tincidunt massa, sed lobortis leo bibendum sed. Donec varius mi vel nulla fringilla, ac congue ligula rhoncus. Aliquam in velit rutrum, pretium nibh quis, aliquam lacus. Integer quis efficitur felis. Morbi laoreet odio eu ultrices rhoncus. Donec eget ultrices est. Vivamus mollis condimentum magna vel lobortis. Praesent condimentum tincidunt euismod. Aliquam eget ornare urna. Phasellus venenatis mi nec tortor scelerisque malesuada. Nam in dolor tincidunt, dapibus risus vel, euismod dolor.",
-                    IMG+"content/images/pcb.jpg"	
+                    IMG+"https://user-images.githubusercontent.com/37296308/75664950-a1934880-5c73-11ea-9baf-e6994173c4f1.jpg"	
                 ]
             },
             {
@@ -45,7 +45,7 @@ function getContent() {
                 paragraphs:[
                     "Pellentesque ut nisi quis dui suscipit convallis nec eget eros. Nam auctor eros non pretium accumsan. Curabitur ut velit at nibh ultrices finibus. Integer in augue varius, gravida tortor non, placerat lorem. Mauris nec lacus et turpis gravida porta quis a leo.",
                     "Donec vulputate arcu sit amet ante pretium, ut condimentum turpis congue. Maecenas placerat erat ac erat vestibulum accumsan. Aliquam sed metus venenatis, consectetur lorem et, euismod eros. Nunc eu ante quis purus tristique accumsan eget et diam. Praesent eu volutpat lorem. Vivamus porttitor odio massa, eget facilisis libero malesuada sit amet. Nunc sit amet ligula aliquam, pharetra nibh ut, vestibulum dolor. Donec consequat lectus vitae lectus blandit, vel euismod urna mollis. Praesent ut cursus nulla.",
-                    IMG+"content/images/surface.jpg"+SUB+"Fusce faucibus mi quis egestas finibus. Pellentesque euismod tincidunt massa, sed lobortis leo bibendum sed",
+                    IMG+"https://user-images.githubusercontent.com/37296308/75664951-a22bdf00-5c73-11ea-8e71-e1c259707c1b.jpg"+SUB+"Fusce faucibus mi quis egestas finibus. Pellentesque euismod tincidunt massa, sed lobortis leo bibendum sed",
                     "Aliquam facilisis consectetur leo, scelerisque pellentesque nunc euismod eu.",
                     "Sed ut risus quam. Phasellus non libero vel mi condimentum volutpat quis at mi. Integer nunc mi, efficitur ac felis quis, ornare scelerisque velit. Nunc bibendum vel augue id pretium."
                 ]
